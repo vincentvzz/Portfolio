9bdeb58d2ca21dd5681080f331d1b9237a8b66e1
vzz <vzz@uw.edu>
2022-05-15 23:14:53 -0700

    try deployment

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,9 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Hi, I'm Vincent.
+            {/* <br className="hidden lg:inline-block" />I love to build amazing
+            apps. */}
           </h1>
           <p className="mb-8 leading-relaxed">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
@@ -28,13 +28,13 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        {/* <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
+            alt="my_photo"
+            src="./img/Vincent.png"
           />
-        </div>
+        </div> */}
       </div>
     </section>
   );
