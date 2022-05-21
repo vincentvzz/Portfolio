f0205c7dd25d7c80eabb0890b22851c5d141bc8d
vzz <vzz@uw.edu>
2022-05-20 19:30:56 -0700

    delete serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
   Navigation,
@@ -31,5 +30,3 @@ ReactDOM.render(
 
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
